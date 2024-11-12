a7f625e73bae23c5e9bbb2251f34837363540f5f
Marmog19 <mario.mogavero02@gmail.com>
2024-11-12 13:12:21 +0100

    Add files via upload

---
+++ BareMetal_SecurityAlarm/Debug/Src/system_stm32f4xx.d
@@ -0,0 +1,17 @@
+Src/system_stm32f4xx.o: ../Src/system_stm32f4xx.c \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/user/STM32CubeIDE/workspace_1.14.0/BareMetal_SecurityAlarm/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ BareMetal_SecurityAlarm/Src/main.c
@@ -0,0 +1,75 @@
+#include <stdint.h>
+#include <stm32f4xx.h>
+
+void GPIO_init(void);
+void delay(int);
+
+int main(void){
+	GPIO_init();
+
+    /* Loop forever */
+	while(1){
+		// check if the toggle (pin 9) is on
+		if (GPIOA->IDR & (1 << 9)) {
+			// check if the armed signals (buttons on pin 6 and 7) are not pressed (1)
+			if ((GPIOA->IDR & (1 << 7)) || (GPIOA->IDR & (1 << 6))) {
+				GPIOA->ODR |= (1<<4); // turns on LED (breadboard)
+				delay(50); // wait 10ms because of button bounce
+			}  else {
+				GPIOA->ODR &= ~(1<<4); // turns off LED (breadboard)
+				delay(50); // wait 10ms because of button bounce
+			}
+		} else {
+			GPIOA->ODR &= ~(1<<4); // turns off LED (breadboard)
+			delay(50); // wait 10ms because of button bounce
+		}
+	}
+}
+
+void GPIO_init(void){
+	//Enable Clock Peripherals (GPIOA)
+	RCC->AHB1ENR |= (1<<0); //Enables GPIOA peripheral (bit 0)
+
+	delay(1); 	// allow time for clock to start (???)
+
+	/// OUTPUT
+	// LED breadboard
+	//Set GPIOA, PIN 4 as Output (MODER[9:8] = 01)
+	GPIOA->MODER &= ~(1<<9); // clear bit 9
+	GPIOA->MODER |= (1<<8); // set bit 8
+	GPIOA->OTYPER &= ~(1<<4); //Sets GPIOA, PIN 4 as push-pull
+	GPIOA->ODR &= ~(1<<4); //Initializes GPIOA, PIN 4 as LOW
+
+	///INPUT
+	// Armed Toggle (ON/OFF)
+	//Set GPIOA, PIN 9 as Input (MODER[19:18] = 00)
+	GPIOA->MODER &= ~(1<<19); // clear bit 19
+	GPIOA->MODER &= ~(1<<18); // clear bit 18
+	//Set GPIOA, PIN 9 as Pull-up (negative logic) (PUPDR[19:18] = 01)
+	GPIOA->PUPDR &= ~(1<<19); // clear bit 19
+	GPIOA->PUPDR |= (1<<18); // set bit 18
+
+	// Button 1
+	//Set GPIOA, PIN 6 as Input (MODER[13:12] = 00)
+	GPIOA->MODER &= ~(1<<13); // clear bit 13
+	GPIOA->MODER &= ~(1<<12); // clear bit 12
+	//Set GPIOA, PIN 6 as Pull-up (negative logic) (PUPDR[13:12] = 01)
+	GPIOA->PUPDR &= ~(1<<13); // clear bit 13
+	GPIOA->PUPDR |= (1<<12); // set bit 12
+
+	// Button 2
+	//Set GPIOA, PIN 7 as Input (MODER[15:14] = 00)
+	GPIOA->MODER &= ~(1<<15); // clear bit 15
+	GPIOA->MODER &= ~(1<<14); // clear bit 14
+	//Set GPIOA, PIN 7 as Pull-up (negative logic) (PUPDR[15:14] = 01)
+	GPIOA->PUPDR &= ~(1<<15); // clear bit 13
+	GPIOA->PUPDR |= (1<<14); // set bit 12
+}
+
+// 1000 = 2 seconds
+void delay(int d){
+	int i;
+	for(; d>0 ;d--){
+		for(i =0; i<2657;i++);
+	}
+}
